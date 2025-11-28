4a01cd9e7ec086caa51b49f9e8290df36d6d8828
Kareem Sabry <2301101@eng.asu.edu.eg>
2025-11-28 20:12:12 +0200

    create test file for community_CRUD.ts

--- utils/community_CRUD.ts
+++ utils/community_CRUD.ts
@@ -1,12 +1,14 @@
 import { Pool } from "pg";
 
-
-export const pool = new Pool({
-   user: "postgres",
-  host: "localhost",
-  database: "postgres",
-  password: "12345678",
-  port: 5432,
+const pool = new Pool({
+    user: 'neondb_owner',
+    host: 'ep-cool-pond-ag6dlbgu-pooler.c-2.eu-central-1.aws.neon.tech',
+    database: 'neondb',
+    password: 'npg_ohLxKP8CJR9U',
+    port: 5432,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 
